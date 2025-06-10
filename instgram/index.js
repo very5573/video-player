3f61400541246272d0faf6b1485d15057d1d6338
very5573 <tripathivery@gmail.com>
2025-06-11 01:29:48 +0530

    Add files via upload

---
+++ instgram/index.js
@@ -0,0 +1,12 @@
+Zindagi ek safar hai jo har mod par naye tajurbe aur seekh deti hai. Kabhi khushi milti  API_Key=221213598515632
+hai, toh kabhi gham; par har lamha ek nayi kahani kehte hai. Insaan har din-ins
+
+// API_Key=221213598515632
+// API_SECRET=as1IzUQxTkrNqMvkqoCnd3dZVbQ
+// SECRET_KEY=hydrogen@1Ca$d!miumXz8
+
+
+kuch na kuch naya seekhta hai – kabhi kisi se milkar, kabhi kisi ko kho kar. Har mushkil  API_SECRET=as1IzUQxTkrNqMvkqoCnd3dZVbQ
+waqt ke baad ek nayi roshni zaroor aati hai, bas yakeen aur himmat banaye rakhni hoti hai.SECRET_KEY=hydrogen@1Ca$d!miumXz8
+ Isi safar ka naam hai zindagi.
+
